126457a3347afa6bd9b593b1cb9e1274b535d816
eksmander <ethan.soehnlin@gmail.com>
2023-02-19 18:27:47 -0500

    fininst

--- Game.gd
+++ Game.gd
@@ -104,4 +104,85 @@ func check_gameover() -> void:
 		is_draw = true
 		add_gameover_message()
 		
-func add_
+func add_gameover_message() -> void:
+	if is_draw:
+		$GameOverMessage/Container/Label.text = "Game is draw!"
+	else:
+		$GameOverMessage/Container/Label.text = "player" + player + "wins!"
+	$GameOverMessage.show()
+	
+func make_move(index: int) -> void:
+	board[index] = player
+	check_gameover()
+	
+func is_square_free(index: int) -> bool:
+	if board[index] == "0":
+		return true
+	return false
+	
+func update_board(row: int, index: int) -> void:
+	var path = "Board/Row" + String(row) + "/Button" + String(index)
+	if player == "x":
+		get_node(path).texture_normal = player_x
+	elif player == "o":
+		get_node(path).texture_normal = player_o
+	update_player()
+	
+func _on_button_0_button_up() -> void:
+	if is_square_free(0) && !is_gameover:
+		make_move(0)
+		update_board(0, 0)
+
+
+func _on_button_1_button_up() -> void:
+	if is_square_free(1) && !is_gameover:
+		make_move(1)
+		update_board(0, 1)
+
+func _on_button_2_button_up() -> void:
+	if is_square_free(2) && !is_gameover:
+		make_move(2)
+		update_board(0, 2)
+
+func _on_button_3_button_up() -> void:
+	if is_square_free(3) && !is_gameover:
+		make_move(3)
+		update_board(1, 3)
+
+
+func _on_button_4_button_up() -> void:
+	if is_square_free(4) && !is_gameover:
+		make_move(4)
+		update_board(1, 4)
+
+
+func _on_button_5_button_up() -> void:
+	if is_square_free(5) && !is_gameover:
+		make_move(5)
+		update_board(1, 5)
+
+
+func _on_button_6_button_up() -> void:
+	if is_square_free(6) && !is_gameover:
+		make_move(6)
+		update_board(2, 6)
+
+
+func _on_button_7_button_up() -> void:
+	if is_square_free(7) && !is_gameover:
+		make_move(7)
+		update_board(2, 7)
+
+
+func _on_button_8_button_up() -> void:
+	if is_square_free(8) && !is_gameover:
+		make_move(8)
+		update_board(2, 8)
+
+
+
+
+func _on_button_button_up() -> void:
+	$GameOverMessage.hide()
+	initiate_board()
+	initiate_player()
